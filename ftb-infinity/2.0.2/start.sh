b7defe8433a729f5a8abac85e47c461fc5182b39
JonasBonno <jonas@bonno.dk>
2015-11-01 15:43:32 +0100

    Update start.sh

--- ftb-infinity/2.0.2/start.sh
+++ ftb-infinity/2.0.2/start.sh
@@ -2,7 +2,7 @@
 
 
 MINECRAFT_HOME="/opt/minecraft"
-FTB_INFINITY_URL="http://ftb.cursecdn.com/FTB2/modpacks/FTBInfinity/2_0_2/FTBInfinityServer.zip"
+FTB_INFINITY_URL="http://addons-origin.cursecdn.com/files/2264/805/FTBInfinityServer.zip"
 LAUNCHWRAPPER="net/minecraft/launchwrapper/1.11/launchwrapper-1.11.jar"
 MINECRAFT_OPTS="-server -Xms2048m -Xmx3072m -XX:MaxPermSize=256m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
 MINECRAFT_STARTUP_JAR="$MINECRAFT_HOME/FTBServer-1.7.10-1448.jar"
