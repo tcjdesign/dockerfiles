884214c6bc02018e692030483b31405671791070
Jonas Bonno Mikkelsen <jonas@bonno.dk>
2016-05-21 14:04:17 +0200

    Update start.sh

--- discordbot/1.4/start.sh
+++ discordbot/1.4/start.sh
@@ -14,6 +14,7 @@ ln -s /data /root/Red-DiscordBot/data
 if [[ ! -d /root/Red-DiscordBot/data/trivia ]]; then
 	echo "data empty"
 	git clone -b develop --single-branch https://github.com/Twentysix26/Red-DiscordBot.git /tmp/Red-DiscordBot
+	cd /root/Red-DiscordBot && pip3 install --upgrade git+https://github.com/Rapptz/discord.py@async
 	cp -R /tmp/Red-DiscordBot/data/* /data
 	rm -Rf /tmp/Red-DiscordBot
 else
