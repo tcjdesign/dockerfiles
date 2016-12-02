fe3f006d2c64d24e5a1c29141bd4268070c8e28f
Jonas Bonno Mikkelsen <jonas@bonno.dk>
2016-12-02 23:21:22 +0100

    Update start.sh

--- discordbot/1.7/start.sh
+++ discordbot/1.7/start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Upgrading pip
-pip3 install --upgrade pip3
-
-# Updates discordbot from Twentysix GitHub
-git pull
+pip3 install --upgrade pip
 
 # Updates discord.py and pip modules
 pip3 install install --upgrade -r requirements.txt
 pip3 install --upgrade imgurpython
+
+# Updates discordbot from Twentysix GitHub
+git pull
 
 # Start python script
 python3.5 red.py
