acaf4bddf3d1206e68aaae3b95fd692b3958456c
Jonas Bonno Mikkelsen <jonas@bonno.dk>
2017-07-13 18:38:33 +0200

    Update start.sh

--- dokuwiki/2017-02-19b/start.sh
+++ dokuwiki/2017-02-19b/start.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-chown -R nobody /var/www
+cd /var/tmp/nginx/fastcgi && chown nobody:root ..
+cd /var/www && chown -R nobody .
 
 su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
-
 exec /usr/bin/supervisord -c /etc/supervisord.conf
